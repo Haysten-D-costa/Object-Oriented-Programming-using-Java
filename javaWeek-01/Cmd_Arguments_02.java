15773794496bdf7a2c56bfd545705ebb5dc4b2f3
Haysten Dcosta <haystendc@gmail.com>
2023-08-16 19:03:01 +0530

    Week 01 work added....

---
+++ javaWeek-01/Cmd_Arguments_02.java
@@ -0,0 +1,11 @@
+/*
+    * The above class takes command line arguments and prints them out.
+*/
+class Cmd_arguments {
+    public static void main(String args[]) {
+        
+        for(String h: args) { // enhanced for loop....
+            System.out.println("String : "+ h);
+        }
+    }
+}
---
+++ javaWeek-01/Input.java
@@ -0,0 +1,18 @@
+/*
+    * This Java program takes two integer inputs from the user, calculates their sum, and displays the
+    * result. We can have other input types like : 
+    *         -> next(), nextLine(), nextFloat(), nextDouble(), nextByte(), etc...
+*/
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in); // created a scanner object....
+        int a = sc.nextInt(); // input a,
+        int b = sc.nextInt(); // input b....
+        int sum = a + b;
+        
+        System.out.println("Sum of numbers : "+ sum); // display the sum....
+    }
+}
